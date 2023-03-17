935401fcdcccfca199c2a00d90d9b812b4a616d5
scnsika <128172508+scnsika@users.noreply.github.com>
2023-03-17 14:53:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.text('🐔 Hard-Boiled Free-RanBreakfast favoritesge Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 
-
+# -----------------------------------------------------------------------------------------------------------
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,5 +26,25 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
-# Nous souhaitons réaliser un filytre sur la liste des fruits et ne proposer que les fruits le splus populaires 
+# -----------------------------------------------------------------------------------------------------------
+# Nous souhaitons réaliser un filtre sur la liste des fruits et ne proposer que les fruits le splus populaires 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+
+# -----------------------------------------------------------------------------------------------------------
+# Nous allons rendre le tableau plus intélligent 
+# de façon à ce qu'il ne filtre que sur les fruits qui sont de;qnés (selectionnés)
+# pour se faire : We'll ask our app to put the list of selected fruits into a variable called fruits_selected. 
+# Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set 
+# (and assign that data to a variable called fruits_to_show).
+# Finally, we'll ask the app to use the data in fruits_to_show in the dataframe it displays on the page. 
+
+
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page 
+streamlit.dataframe(fruits_to_show)
+
+
+
