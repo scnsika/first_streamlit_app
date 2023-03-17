f6e3bf0cfb89f184ac3303309deef17313ddaa65
scnsika <128172508+scnsika@users.noreply.github.com>
2023-03-17 13:46:01 -0400

    Update streamlit_app.py
    Correction de certains termes et ajout d'émoticon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,17 @@ import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Bluberry Oatmeat')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Madessou na losss')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-                
-                
-                
-                
-                
-                
-                
-                
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-RanBreakfast favoritesge Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+
