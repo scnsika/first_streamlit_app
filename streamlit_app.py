3150ff0e4a5b955ba76c529a22c55c1246bd5957
scnsika <128172508+scnsika@users.noreply.github.com>
2023-03-17 14:59:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 # (and assign that data to a variable called fruits_to_show).
 # Finally, we'll ask the app to use the data in fruits_to_show in the dataframe it displays on the page. 
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 
 
 
